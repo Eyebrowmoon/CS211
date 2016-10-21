ec305cdb75e37a17043c48b715d0f475a3381495
Eyebrowmoon <khpark0312@postech.ac.kr>
2016-10-22 04:33:45 +0900

    csim done

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -2,8 +2,8 @@
 #include "csim.h"
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <getopt.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[])
@@ -19,7 +19,9 @@ int main(int argc, char *argv[])
     exit(0);
   }
 
-  printSummary(0, 0, 0);
+  simulate(csim);
+
+  printSummary(csim->hit_count, csim->miss_count, csim->eviction_count);
   return 0;
 }
 
@@ -48,12 +50,11 @@ struct cache_set *new_cset(int E)
   return cset; 
 }
 
-/* Return new cache simulator.
- * calloc used instead of malloc,
- * since 0 is the default value for all members.*/
 struct cache_sim *new_csim(void)
 {
-  struct cache_sim *csim = calloc(sizeof(struct cache_sim), 1); 
+  struct cache_sim *csim = malloc(sizeof(struct cache_sim)); 
+
+  memset(csim, 0, sizeof(struct cache_sim));
 
   return csim;
 }
@@ -107,16 +108,80 @@ void free_csim(struct cache_sim *csim)
 void add_line(struct cache_set *cset)
 {
   struct cache_line *cline = malloc(sizeof(struct cache_line));
+
+  cline->valid = 0;
+  prepend_line(cset, cline);
+}
+
+/* cset must be nonempty */
+void unlink_line(struct cache_line *cline)
+{
+  struct cache_line *next = cline->next;
+  struct cache_line *prev = cline->prev;
+
+  next->prev = prev;
+  prev->next = next;
+}
+
+void prepend_line(struct cache_set *cset, struct cache_line *cline)
+{
   struct cache_line *head = cset->head;
   struct cache_line *head_next = head->next;
 
-  cline->valid = 0;
-  
   cline->prev = head;
   cline->next = head_next;
 
   head->next = cline;
-  head_next->prev = cline;
+  head_next->prev = cline; 
+}
+
+struct cache_line *
+find_line(struct cache_set *cset, uint64_t tag)
+{
+  struct cache_line *cur_line = cset->head->next;
+  struct cache_line *tail = cset->tail;
+
+  while (cur_line != tail) {
+    if (cur_line->valid && cur_line->tag == tag)
+      return cur_line;
+
+    cur_line = cur_line->next;
+  }
+
+  return NULL;
+}
+
+struct cache_line *
+find_invalid(struct cache_set *cset)
+{
+  struct cache_line *cur_line = cset->head->next;
+  struct cache_line *tail = cset->tail;
+
+  while (cur_line != tail) {
+    if (!cur_line->valid)
+      return cur_line;
+
+    cur_line = cur_line->next;
+  }
+
+  return NULL;
+}
+
+
+void print_help(void)
+{
+  printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+  printf("Options:\n");
+  printf("  -h         Print this help message.\n");
+  printf("  -v         Optional verbose flag.\n");
+  printf("  -s <num>   Number of set index bits.\n");
+  printf("  -E <num>   Number of lines per set.\n");
+  printf("  -b <num>   Number of block offset bits.\n");
+  printf("  -t <file>  Trace file.\n\n");
+      
+  printf("Examples:\n");
+  printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+  printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
 }
 
 /* Parse arguments and store to csim, and return it. */
@@ -127,7 +192,7 @@ struct cache_sim *parse_args(int argc, char *argv[])
   int opt = 0;
   char *trace_name;
 
-  while (-1 != (opt = getopt(argc, argv, "hvsEbt:"))) {
+  while (-1 != (opt = getopt(argc, argv, "hvs:E:b:t:"))) {
     switch(opt) {
       case 'h':
         print_help();
@@ -149,6 +214,7 @@ struct cache_sim *parse_args(int argc, char *argv[])
         strcpy(trace_name, optarg);
                
         csim->trace_name = trace_name;
+        break;
     }
   }
 
@@ -160,19 +226,82 @@ int is_valid_input(struct cache_sim *csim)
   return (csim->s > 0 && csim->E > 0 && csim->b > 0 && csim->trace_name);
 }
 
-void print_help(void)
+void access_cache(struct cache_sim *csim, uint64_t addr, int size)
 {
-  printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
-  printf("Options:\n");
-  printf("  -h         Print this help message.\n");
-  printf("  -v         Optional verbose flag.\n");
-  printf("  -s <num>   Number of set index bits.\n");
-  printf("  -E <num>   Number of lines per set.\n");
-  printf("  -b <num>   Number of block offset bits.\n");
-  printf("  -t <file>  Trace file.\n\n");
-      
-  printf("Examples:\n");
-  printf("  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n");
-  printf("  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+  int s = csim->s, b = csim->b;
+  int verbose = csim->verbose;
+
+  uint64_t tag = addr >> (s + b);
+  int set_idx = (addr >> b) & ((1 << s) - 1);
+
+  struct cache_set *cset = csim->cache[set_idx];
+  struct cache_line *cline = find_line(cset, tag);
+
+  if (cline) {
+    if (verbose)
+      printf(" hit");
+
+    unlink_line(cline);
+    prepend_line(cset, cline);
+
+    csim->hit_count++;
+  }
+  else {
+    if (verbose)
+      printf(" miss");
+
+    cline = find_invalid(cset);
+
+    // Eviction
+    if (!cline) {
+      if (verbose)
+        printf(" eviction");
+
+      cline = cset->tail->prev;
+      csim->eviction_count++;
+    }
+
+    cline->valid = 1;
+    cline->tag = tag;
+
+    unlink_line(cline);
+    prepend_line(cset, cline);
+
+    csim->miss_count++;
+  }
+}
+
+void simulate(struct cache_sim *csim)
+{
+  FILE *f;
+  char id;
+  unsigned long long addr;
+  int size;
+
+  int verbose = csim->verbose;
+
+  f = fopen(csim->trace_name, "r");
+
+  while(fscanf(f, " %c %llx,%d\n", &id, &addr, &size) > 0) {
+    
+    if (verbose && id != 'I')
+      printf("%c %llx,%d", id, addr, size);
+
+    switch (id) {
+      case 'L':
+      case 'S':
+        access_cache(csim, (uint64_t)addr, size);
+        break;
+      case 'M':
+        access_cache(csim, (uint64_t)addr, size);
+        access_cache(csim, (uint64_t)addr, size);
+        break;
+    }
+
+    if (verbose)
+      printf("\n");
+  }
+
+  fclose(f);
 }
 
--- cachelab/csim.h
+++ cachelab/csim.h
@@ -1,7 +1,10 @@
 #ifndef _CSIM_H
 #define _CSIM_H
 
-#include <stdint.h>
+#include <inttypes.h>
+
+#define HIT 0
+#define MISS
 
 struct cache_line {
   int valid;
@@ -24,6 +27,10 @@ struct cache_sim {
   int b;
   char *trace_name;
 
+  int hit_count;
+  int miss_count;
+  int eviction_count;
+
   struct cache_set **cache;
 };
 
@@ -34,11 +41,17 @@ void free_cset(struct cache_set *cset);
 void free_csim(struct cache_sim *csim);
 
 void add_line(struct cache_set *cset);
-//struct cache_line *exist_line(uint64_t tag);
+void unlink_line(struct cache_line *cline);
+void prepend_line(struct cache_set *cset, struct cache_line *cline);
+struct cache_line *find_line(struct cache_set *cset, uint64_t tag);
+struct cache_line *find_invalid(struct cache_set *cset);
+
+void print_help(void);
 
 struct cache_sim *parse_args(int argc, char *argv[]);
 int is_valid_input(struct cache_sim *csim);
 
-void print_help(void);
+void access_cache(struct cache_sim *csim, uint64_t addr, int size); 
+void simulate(struct cache_sim *csim);
 
 #endif
